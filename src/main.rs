0c1b6aaad80062427bf2288e48bb0f879c7f2207
ChunPanYung <31672021+ChunPanYung@users.noreply.github.com>
2025-11-08 13:29:10 -0500

    feat: create an example as base and args functionality. (#2)
    * test: get args and print.
    
    * doc: readme file.
    
    * feat: basic parser.
    
    * feat: add `clap` dependencies.
    
    * fix: enable 'derive' feature from `clap`.
    
    * test: get file name.
    
    * doc: setup taskfile script.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+/// Binary Manager, it will install or update binary from GitHub, CLI, or Bash Script.
+#[derive(Parser)]
+#[command(name = "binman")]
+#[command(version = "0.1")]
+#[command(about = "Install or update binary from GitHub, CLI, or Bash Script.")]
+struct Cli {
+    /// Configuration file path
+    #[arg(short, long, value_name = "FILE")]
+    file: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    // dbg!(args);
+
+    println!("{}", cli.file);
 }
